bcb5c5f41516f69d72dc838693ff33bac620084e
준모 김 <kimjunmo91@naver.com>
2020-10-13 09:58:14 +0900

    파일 리스트 보기 완료

--- Dorrent/Form2.Designer.cs
+++ Dorrent/Form2.Designer.cs
@@ -28,19 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.bt_folder = new System.Windows.Forms.Button();
+            this.lb_choosen = new System.Windows.Forms.Label();
+            this.lb_folder = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
+            // 
+            // bt_folder
+            // 
+            this.bt_folder.Location = new System.Drawing.Point(13, 13);
+            this.bt_folder.Name = "bt_folder";
+            this.bt_folder.Size = new System.Drawing.Size(75, 23);
+            this.bt_folder.TabIndex = 0;
+            this.bt_folder.Text = "폴더 선택";
+            this.bt_folder.UseVisualStyleBackColor = true;
+            this.bt_folder.Click += new System.EventHandler(this.bt_folder_Click);
+            // 
+            // lb_choosen
+            // 
+            this.lb_choosen.AutoSize = true;
+            this.lb_choosen.Location = new System.Drawing.Point(13, 50);
+            this.lb_choosen.Name = "lb_choosen";
+            this.lb_choosen.Size = new System.Drawing.Size(77, 12);
+            this.lb_choosen.TabIndex = 1;
+            this.lb_choosen.Text = "선택된 폴더 :";
+            // 
+            // lb_folder
+            // 
+            this.lb_folder.AutoSize = true;
+            this.lb_folder.Location = new System.Drawing.Point(96, 50);
+            this.lb_folder.Name = "lb_folder";
+            this.lb_folder.Size = new System.Drawing.Size(29, 12);
+            this.lb_folder.TabIndex = 2;
+            this.lb_folder.Text = "없음";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 70);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 23;
+            this.dgv.Size = new System.Drawing.Size(960, 380);
+            this.dgv.TabIndex = 3;
             // 
             // Form_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.lb_folder);
+            this.Controls.Add(this.lb_choosen);
+            this.Controls.Add(this.bt_folder);
             this.Name = "Form_main";
             this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bt_folder;
+        private System.Windows.Forms.Label lb_choosen;
+        private System.Windows.Forms.Label lb_folder;
+        private System.Windows.Forms.DataGridView dgv;
     }
 }
--- Dorrent/Form2.cs
+++ Dorrent/Form2.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Dorrent
 {
@@ -15,6 +18,64 @@ namespace Dorrent
         public Form_main()
         {
             InitializeComponent();
+        }
+
+        private void bt_folder_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if(dialog.ShowDialog()==CommonFileDialogResult.Ok)
+            {
+                lb_folder.Text = dialog.FileName;
+                DataTable dt_filelist = FileList(dialog.FileName);
+                ShowDataTable(dt_filelist, dgv);
+            }
+        }
+
+        private DataTable FileList(string FolderName)
+        {
+            DirectoryInfo di = new DirectoryInfo(FolderName);
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Folder", typeof(string));
+            dt.Columns.Add("FileName", typeof(string));
+            dt.Columns.Add("Extension", typeof(string));
+            dt.Columns.Add("CreationTime", typeof(DateTime));
+            dt.Columns.Add("LastWriteTime", typeof(DateTime));
+            dt.Columns.Add("LastAccessTime", typeof(DateTime));
+
+            foreach(FileInfo File in di.GetFiles())
+            {
+                dt.Rows.Add(File.DirectoryName, File.Name, File.Extension, File.CreationTime, File.LastWriteTime, File.LastAccessTime);
+            }
+
+            return dt;
+        }
+
+        private void ShowDataTable(DataTable dt, DataGridView dgv)
+        {
+            dgv.Rows.Clear();
+            dgv.Columns.Clear();
+
+            foreach(DataColumn dc in dt.Columns)
+            {
+                dgv.Columns.Add(dc.ColumnName, dc.ColumnName);
+            }
+
+            int row_index = 0;
+            foreach(DataRow dr in dt.Rows)
+            {
+                dgv.Rows.Add();
+                foreach(DataColumn dc in dt.Columns)
+                {
+                    dgv.Rows[row_index].Cells[dc.ColumnName].Value = dr[dc.ColumnName];
+                }
+                row_index++;
+            }
+
+            foreach(DataGridViewColumn drvc in dgv.Columns)
+            {
+                drvc.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            }
         }
     }
 }
